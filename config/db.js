0ada090a247d7edb19ea69ff5a02de33c0c92a8c
MarcosBoni <AngelssGroup@proton.me>
2024-12-03 12:26:45 -0300

    db

--- config/db.js
+++ config/db.js
@@ -1,14 +1,18 @@
+require('dotenv').config();  // Carrega as variáveis do .env
+
 const { Client } = require('pg');
 
+// Criando o cliente de conexão com o banco de dados
 const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'loja_catolica',
-    password: 'admin',
-    port: 5432,
-    connectionTimeoutMillis: 10000, // Aumenta o tempo de conexão para 10 segundos
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,  // Normalmente 5432
+    connectionTimeoutMillis: 10000,  // Aumenta o tempo de conexão para 10 segundos
 });
 
+// Conectando ao banco de dados
 client.connect()
     .then(() => {
         console.log('Conexão com o banco de dados estabelecida com sucesso');
