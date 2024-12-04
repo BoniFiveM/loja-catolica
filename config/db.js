140a73644ea2413b5afef97721b7d24eb888057e
MarcosBoni <AngelssGroup@proton.me>
2024-12-04 19:12:59 -0300

    tet

--- config/db.js
+++ config/db.js
@@ -1,24 +1,18 @@
-require('dotenv').config();  // Carrega as variáveis do .env
-
 const { Client } = require('pg');
 
-// Criando o cliente de conexão com o banco de dados
 const client = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,  // Normalmente 5432
-    connectionTimeoutMillis: 10000,  // Aumenta o tempo de conexão para 10 segundos
+    connectionString: 'postgresql://loja_catolica_owner:Ewv6Q8ruLIJk@ep-rough-scene-a5i58s64.us-east-2.aws.neon.tech/loja_catolica?sslmode=require',
+    ssl: {
+        rejectUnauthorized: false // Necessário para conexões seguras no Neon
+    },
 });
 
-// Conectando ao banco de dados
 client.connect()
     .then(() => {
-        console.log('Conexão com o banco de dados estabelecida com sucesso');
+        console.log('Conexão com o banco de dados Neon estabelecida com sucesso');
     })
     .catch(err => {
-        console.error('Erro ao conectar com o banco de dados:', err);
+        console.error('Erro ao conectar com o banco de dados Neon:', err);
     });
 
 module.exports = client;
